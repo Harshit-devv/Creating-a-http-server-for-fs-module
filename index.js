4979174874424552150ad861245cb54bfc0157fc
Harshit-devv <harshitsharma404@gmail.com>
2024-08-17 04:11:21 +0530

    Created routed as per the requirements

---
+++ index.js
@@ -0,0 +1,59 @@
+/**
+ You need to create an express HTTP server in Node.js which will handle the logic of a file server.
+- Use built in Node.js `fs` module
+The expected API endpoints are defined below,
+1. GET /files - Returns a list of files present in `./files/` directory
+Response: 200 OK with an array of file names in JSON format.
+Example: GET http://localhost:3000/files
+2. GET /file/:filename - Returns content of given file by name
+    Description: Use the filename from the request path parameter to read the file from `./files/` directory
+    Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
+    Example: GET http://localhost:3000/file/example.txt
+- For any other route not defined in the server return 404
+Testing the server - run `npm run test-fileServer` command in terminal
+*/
+
+const express = require('express');
+const fs = require('fs');
+const port = 2002;
+const path = require('path');
+const app = express();
+
+app.get('/files', (req, res) => {
+    const directoryPath = path.join(__dirname, '/files');
+  
+    fs.readdir(directoryPath, (err, files) => {
+      if (err) {
+        return res.status(500).json({ message: 'Unable to scan directory', error: err.message });
+      }
+  
+      res.status(200).json(files);
+    });
+  });
+
+
+app.get('/file/:filename', (req, res) => {
+    const fileName = req.params.filename;
+    const filePath = path.join(__dirname, '/files', fileName);
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          if (err.code === 'ENOENT') {
+            return res.status(404).send('File not found');
+          }
+          return res.status(500).json({ message: 'Unable to read file', error: err.message });
+        }
+    
+        res.status(200).send(data);
+    });
+})
+
+// Handle undefined routes
+app.use((req, res) => {
+    res.status(404).send('Not Found');
+  });
+  
+
+// Listining on port  
+app.listen(port, () => {
+    console.log(`The Server is running on http://localhost:${port}`);
+});
